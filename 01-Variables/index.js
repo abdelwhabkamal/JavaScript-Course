b83ff35d502c1505f5b77f29edad23bd529e413a
abdelwhabkamal <abdewhabkamal17@gmail.com>
2023-11-15 12:03:12 +0200

    Variables

---
+++ 01-Variables/index.js
@@ -0,0 +1,13 @@
+var FName="Abdelwahab";
+var LName="Kamal";
+var xy="My Age is:";
+var FullName=FName+" "+LName;
+
+//works in specific scoop only 
+let temp="My Name is:";
+
+//constant value can't change
+const age=22;
+
+console.log(temp,FullName);
+console.log(xy,age);
