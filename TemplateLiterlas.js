fbb1c12d1adb8da63d467342a68d1c88f6b1f6a1
abdelwhabkamal <abdewhabkamal17@gmail.com>
2023-11-22 10:14:35 +0200

    Template Literals

---
+++ TemplateLiterlas.js
@@ -0,0 +1,9 @@
+let Fname="Abdelwahab";
+let Lname="Kamal";
+let Salary=17000;
+let tax=22;
+let total;
+console.log(`Hello ${Fname} ${Lname}`);
+console.log(`Your Salary: ${Salary} EGP`);
+console.log(`Tax: ${tax*Salary/100}`);
+console.log(`Net Salary: ${total=Salary-tax*Salary/100} EGP`);
